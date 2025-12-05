5bcd5c5501c9f9963d2c5ba19b1772ff41f59e3f
xiaoli <2585960098@qq.com>
2025-12-05 10:22:30 +0800

    chore: note

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,6 +2,6 @@ alias reload='. ~/.zshrc'
 alias ls="eza -la"
 alias vim="nvim"
 alias cat="bat"
-alias date='gdate "+%Y-%m-%d %H:%M:%S %A"'
+alias date='gdate "+%Y-%m-%d %H:%M:%S %A"'  # brew install coreutils
 alias dt='date'
 
