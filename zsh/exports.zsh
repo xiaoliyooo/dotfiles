8074c1b69201cf7253c8c32d50a946f7734fa5e7
xiaoliyo <chezemin@192.168.1.4>
2025-12-07 14:55:38 +0800

    chore: exports

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -6,3 +6,8 @@ export EZA_CONFIG_DIR="$HOME/.config/nvim/eza/themes"
 
 export MCFLY_KEY_SCHEME=emacs # 主要为了<C-w>删除单词
 export MCFLY_FUZZY=2
+
+export EDITOR=nvim
+
+# export MANPAGER="sh -c 'col -bx | bat -l man -p'"
+export MANPAGER="nvim +Man!"
